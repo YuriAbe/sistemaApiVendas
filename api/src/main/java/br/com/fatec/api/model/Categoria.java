f0595ad13303d39628a3cf859777a4e8271bc80d
YuriAbe <yuriabe35@gmail.com>
2026-04-14 02:31:18 -0300

    fix: Erros da API no post e update de categorias resolvido

--- api/src/main/java/br/com/fatec/api/model/Categoria.java
+++ api/src/main/java/br/com/fatec/api/model/Categoria.java
@@ -2,6 +2,8 @@ package br.com.fatec.api.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,6 +26,7 @@ public class Categoria {
     private String nomeCategoria;
 
     @OneToMany(mappedBy = "categoria")
+    @JsonIgnore
     private List<Produto> produtos;
 
 }
